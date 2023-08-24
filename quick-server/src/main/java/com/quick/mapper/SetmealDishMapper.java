9457c13b9981305932c704609a0b689797171f4a
BennettMa23 <19906406473@163.com>
2023-08-24 11:13:23 +0800

    套餐业务代码

--- quick-server/src/main/java/com/quick/mapper/SetmealDishMapper.java
+++ quick-server/src/main/java/com/quick/mapper/SetmealDishMapper.java
@@ -3,6 +3,7 @@ package com.quick.mapper;
 import com.quick.entity.SetmealDish;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.mybatis.spring.annotation.MapperScan;
 
 import java.util.List;
@@ -32,4 +33,11 @@ public interface SetmealDishMapper {
      */
     @Delete("delete from setmeal_dish where setmeal_id = #{setmealId}")
     void deleteBySetmealId(Long setmealId);
+    /**
+     * 根据套餐id查询套餐和菜品的关联关系
+     * @param setmealId
+     * @return
+     */
+    @Select("select * from setmeal_dish where setmeal_id = #{setmealId}")
+    List<SetmealDish> getBySetmealId(Long setmealId);
 }
